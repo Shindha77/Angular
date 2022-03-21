b59cf95554b1184cb965a53c41e986637e5e4bc9
Jérémy Chalumeau <jchalumeau77@gmail.com>
2022-03-21 16:15:59 +0100

    bindings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BlockComponent } from './block/block.component';
 
 const routes: Routes = [];
 
